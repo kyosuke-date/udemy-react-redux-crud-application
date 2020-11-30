95f28718f60f97212f3551422efa4333cf9a8962
kyosuke-date <k.date1009@gmail.com>
2020-11-30 22:10:44 +0900

    state practice

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import './App.css';
+import React from 'react';
 
 //function App() {
 //  return (
 //    <div>Hello, World</div>
 //  );
-}//
+//}
 
-const App = () => {
-  return <div>Hi!</div>
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
+
+  numPlus = () => {
+    const int = this.state.count;
+    this.setState({ count: int + 1 });
+  }
+  numMinus = () => {
+    const int = this.state.count;
+    this.setState({ count: int - 1 });
+  }
+
+  render(){
+    return(
+      <div>
+        <p>{this.state.count}</p>
+        <button onClick={this.numPlus}>+1</button>
+        <button onClick={this.numMinus}>-1</button>
+      </div>
+    )
+  }
 }
 export default App;
