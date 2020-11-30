261fc97560390678b5ac39ff762f96d08633e152
kyosuke-date <k.date1009@gmail.com>
2020-11-30 21:14:32 +0900

    Sample

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 
-function App() {
-  return (
-    <div>Hello, World</div>
-  );
-}
+//function App() {
+//  return (
+//    <div>Hello, World</div>
+//  );
+}//
 
+const App = () => {
+  return <div>Hi!</div>
+}
 export default App;
